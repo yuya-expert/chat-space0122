03be9655567372c6c0715b02dadc87b6d91390c5
yuya-expert <yuya@yuuyanoMacBook-puro.local>
2019-01-21 17:46:49 +0900

    {}を外し、省略形に変更。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -28,7 +29,7 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, user_ids: [])
   end
 
   def set_group
