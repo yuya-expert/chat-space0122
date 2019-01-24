ed5833732b313578a8b4f29da6db0974304ddf4e
yuya-expert <yuya@yuuyanoMacBook-puro.local>
2019-01-24 16:41:54 +0900

    自動更新の処理（JS）

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function(){
     var imgHTML = message.image ? `<img src="${message.image}">`: ``;
 
     var html =
-      `<div class = "chat-main__body--messages-list">
+      `<div class = "chat-main__body--messages-list" data-id = "${message.id}">
       <div class = "chat-main__message--clearfix">
         <div class = "chat-main__message--name">
           ${message.user_name}
@@ -13,8 +13,8 @@ $(function(){
           ${message.created_at}
         </div>
         <div class = "chat-main__message--body">
-          <p>${message.content}</p>
-          <p>${imgHTML}</p>
+          <div>${message.content}</div>
+            ${imgHTML}
         </div>
       </div>`;
     return html;
@@ -32,6 +32,7 @@ $(function(){
       processData: false,
       contentType: false
     })
+
     // うまくいった時はdoneが送信される
     .done(function(data){
       var html = buildHTML(data);
@@ -41,6 +42,7 @@ $(function(){
       $('.form__message').val('')
       $(message_image).val('')
       $(".chat-main__body").animate({scrollTop:100000});
+      console.log('1')
     })
     // うまくいかなかった時はfailが送信される。
     .fail(function(){
@@ -48,5 +50,40 @@ $(function(){
       $('input').prop('disabled', false);
     })
   })
-});
 
+  // 自動通信の処理
+  $(function(){
+    setInterval(update, 5000);
+    //10000ミリ秒ごとにupdateという関数を実行する
+    function update(){ //この関数では以下のことを行う
+        var message_id = $('.chat-main__body--messages-list:last').data('id'); //一番最後にある'chat-main__message--time'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
+      if (location.pathname.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({ //ajax通信で以下のことを行う
+        url: location.href, //urlは現在のページを指定
+        type: 'GET', //メソッドを指定
+        data: { id: message_id }, //このような形(paramsの形をしています)で、'id'には'message_id'を入れる
+        dataType: 'json' //データはjson形式
+      })
+      // うまくいった時はdoneが送信される
+      .done(function(data){
+        console.log(data)
+        console.log(data.length)
+        if (data.length != 0){
+        data.forEach(function(message) {
+        var html = buildHTML(message);
+        // 指定したクラスに対して、要素を追加していく
+        $('.chat-main__body').append(html)
+        $('.chat-main__body').animate({scrollTop:100000});
+        console.log('2')
+      });
+      }
+    })
+      // うまくいかなかった時はfail_が送信される
+          .fail(function(){
+        alert('error');
+        $('input').prop('disabled', false);
+      })
+    }
+   }
+  });
+});
