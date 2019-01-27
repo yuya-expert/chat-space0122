f2ae1ee2a9a189e70a3ab0e63bd5f159b9239650
yuya-expert <yuya@yuuyanoMacBook-puro.local>
2019-01-27 14:14:13 +0900

    s3実装

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'yuya-araki2'
+  config.fog_directory  = 'yuya-araki'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/yuya-araki2'
 end
